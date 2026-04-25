a2bbaed9be84c7ae3c3ec748601fed580be87846
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-04-25 16:43:28 +0530

    Grabage

---
+++ PythonMCA/program44.py
@@ -0,0 +1,17 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Name - Swati Yadav ")
+... print(" Class - MCA 2nd ")
+... print("Roll No - 52")
+... def factorial(n):
+...     if n == 0 or n == 1:
+...         return 1
+...     return n * factorial(n - 1)
+... 
+... num = int(input("Enter a number: "))
+... print("Factorial:", factorial(num))
+>>> [DEBUG ON]
+>>> [DEBUG OFF]
+>>> 
+>>> [DEBUG ON]
+>>> [DEBUG OFF]
